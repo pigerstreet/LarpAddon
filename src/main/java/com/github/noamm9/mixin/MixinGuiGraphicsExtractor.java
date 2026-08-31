6b5bb64b17940960b8336739b8863df49593b7f7
pigerstreet <whuazemc@gmail.com>
2026-08-31 10:35:14 +1000

    Scale storage overlay tooltips

--- src/main/java/com/github/noamm9/mixin/MixinGuiGraphicsExtractor.java
+++ src/main/java/com/github/noamm9/mixin/MixinGuiGraphicsExtractor.java
@@ -2,6 +2,7 @@ package com.github.noamm9.mixin;
 
 import com.github.noamm9.features.impl.dev.text.TextReplacer;
 import com.github.noamm9.features.impl.general.ItemTooltip;
+import com.github.noamm9.features.impl.general.storageoverlay.StorageOverlayTooltip;
 import com.github.noamm9.features.impl.misc.Tweaks;
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
@@ -27,12 +28,14 @@ public abstract class MixinGuiGraphicsExtractor {
     @WrapMethod(method = "tooltip")
     private void onRenderTooltipPre(Font font, List<ClientTooltipComponent> lines, int xo, int yo, ClientTooltipPositioner positioner, @org.jspecify.annotations.Nullable Identifier style, Operation<Void> original) {
         TextReplacer.tooltip = true;
-        if (! ItemTooltip.isScrollingEnabled()) original.call(font, lines, xo, yo, positioner, style);
+        boolean scrolling = ItemTooltip.isScrollingEnabled();
+        float storageScale = StorageOverlayTooltip.scale(); /// fork: matches the storage overlay
+        if (! scrolling && storageScale == 1f) original.call(font, lines, xo, yo, positioner, style);
         else {
             pose.pushMatrix();
             pose.translate(xo, yo);
-            pose.scale(ItemTooltip.getTooltipScale().getValue().floatValue() / 100f + ItemTooltip.scaleOverride / 10f);
-            pose.translate(ItemTooltip.scrollAmountX, ItemTooltip.scrollAmountY);
+            pose.scale((scrolling ? ItemTooltip.getTooltipScale().getValue().floatValue() / 100f + ItemTooltip.scaleOverride / 10f : 1f) * storageScale);
+            if (scrolling) pose.translate(ItemTooltip.scrollAmountX, ItemTooltip.scrollAmountY);
             pose.translate(- xo, - yo);
             original.call(font, lines, xo, yo, positioner, style);
             pose.popMatrix();
